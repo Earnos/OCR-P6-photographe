ec63f07f66d71fe48adf9d9604cd890703fedeb4
Earnos <lcyril33@gmail.com>
2023-07-25 09:39:45 +0200

    display header data

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -9,33 +9,29 @@ const ID = searchParams.get("photographer-id")
 /**
  * Fecth photographers
  * with local json data
- * @returns {Promise} photographersData - stringify json's data
+ * @returns {Object} photographersData - stringify json's data
  */
 async function getPhotographers() {
   const url = "../../data/photographers.json"
 
   let photographersData = await fetch(url)
   const res = await photographersData.json()
-  //.then((response) => response.json())
-  //.then((response) => (res = JSON.stringify(response)))
-  //.catch((error) => error)
+  let p
   res.photographers.forEach((photographer) => {
     if (photographer.id == ID) {
-      //console.log(photographer.id)
-      return res.photographer
+      p = photographer
     }
   })
+  return p
 }
 
 async function displayPhotographersData(photographersData) {
   const photographProfil = document.querySelector(".photograph-header")
 
-  photographersData.forEach((photographer) => {
-    const photographerModel = onePhotographTemplate(photographer)
-    const userProfilDOM = photographerModel.getPhotographHeaderDOM()
-
-    photographProfil.appendChild(userProfilDOM)
-  })
+  console.log(photographersData)
+  const photographerModel = photographerFactory(photographersData)
+  const userProfilDOM = photographerModel.getPhotographHeaderDOM()
+  photographProfil.appendChild(userProfilDOM)
 }
 
 // async function displayPhotographerMedia(photographersData) {
@@ -51,48 +47,8 @@ async function displayPhotographersData(photographersData) {
 
 async function init() {
   // Récupère les datas d'un photographe
-  const { photographers } = await getPhotographers()
+  const photographers = await getPhotographers()
+  console.log(photographers)
   displayPhotographersData(photographers)
 }
 init()
-
-// get url's ID
-// async function getPhotographData() {
-//   const url = "../../data/photographers.json"
-//   let photographData = await fetch(url)
-//     .then((response) => response.json())
-//     .then((response) => JSON.stringify(response))
-//     .catch((error) => error)
-
-//   return JSON.parse(photographData)
-// }
-
-// const photographersData = await fetch(url)
-// .then((response) => response.json())
-// .then((response) => JSON.stringify(response)
-// .catch((error) => error)
-// const response = await fetch(url)
-// const res = await response.json()
-// const id = getUrlId()
-
-// return JSON.parse(photographersData)
-// creation of photographer's profil header DOM elements
-
-// function headerDisplay() {
-//   const photographProfil = document.querySelector(".photograph-header")
-//   const photographerInfoContent = document.createElement("div")
-
-//   const h2 = document.createElement("h2")
-//   h2.textContent = name
-//   console.log(photographers[0])
-//   const locality = document.createElement("h3")
-//   locality.textContent = city + "," + " " + country
-//   const description = document.createElement("p")
-//   description.textContent = tagline
-
-//   photographProfil.appendChild(photographerInfoContent)
-//   photographerInfoContent.appendChild(h2, locality, description)
-// }
-// function profilDisplay() {
-//   headerDisplay()
-// }
